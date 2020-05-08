53795577831a97c24e9c178cb0ca5c6c534bc743
Nick Gonzalez <gonzaleznick5@gmail.com>
2020-05-07 21:42:30 -0400

    01_Nav_and_loging

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import SignUp from './components/onBoarding/signUp';
+import Nav from './components/nav_bar/nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Nav></Nav>
         <p>
           Hashnbet
         </p>
+        <SignUp></SignUp>
       </header>
     </div>
   );
